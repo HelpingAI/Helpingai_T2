1ea09c4cfa62745913930b9404b9234b6b55060e
Vortex <138214630+HelpingAI@users.noreply.github.com>
2024-01-11 17:26:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="Helpingai_T2",
-    version="0.2",
+    version="0.3",
     description="A simple module to use Perplexity AI in Python.",
     long_description=README,
     long_description_content_type="text/markdown",
